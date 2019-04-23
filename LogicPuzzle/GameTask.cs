204b72083d1b2a9e63bba87b54390c2ce98d0aca
Simon Gockner <simon.gockner@gmail.com>
2019-04-23 22:11:19 +0200

    - add base game task

---
+++ LogicPuzzle/GameTask.cs
@@ -0,0 +1,25 @@
+﻿
+namespace LogicPuzzle
+{
+    public class GameTask
+    {
+        private bool _running;
+
+        public GameTask()
+        {
+            
+        }
+
+        public void Run()
+        {
+            if (_running)
+                return;
+
+            _running = true;
+            while (_running)
+            {
+                
+            }
+        }
+    }
+}
